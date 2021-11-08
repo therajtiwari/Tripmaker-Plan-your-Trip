6222a57b032614cc202aa79a5c8fa4375e1fd876
AxonBlAzE <s.mulkutkar@somaiya.edu>
2021-11-08 22:17:37 +0530

    completed add_booking_order

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 include("stripe-php-master/init.php");
 $publishable_key = 'pk_test_51JtYKqSEwXJXrr8JMduWP5TgFWps1ackPZRzQOpf3jizRedAaNwRkKfiu0L93qF1rcwyzzNh7MFxmkxT87do8NjV00kjR8bJ9m';
-$secret_key = 'sk_test_61JtYKqSEwXJXrr8JRoH5H5Fa1lFzYV2rAw7oQZGN9M3Qs0E3uCN1ssDbZYB5wj0Z2Fn0X5XkIrlah235rUEanrPL00vXp6zOL5';
+$secret_key = 'sk_test_51JtYKqSEwXJXrr8JRoH5H5Fa1lFzYV2rAw7oQZGN9M3Qs0E3uCN1ssDbZYB5wj0Z2Fn0X5XkIrlah235rUEanrPL00vXp6zOL6';
 
 
 
--- payment_success.php
+++ payment_success.php
@@ -1,6 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php
+function add_booking_order(){$conn = mysqli_connect("localhost","root","","travels","3306");
+$tour_package_id = strval($_COOKIE['package_id']);
+$email = $_COOKIE['email'];
+$sql = "SELECT id FROM `user_info` WHERE `email` = '$email'";
+$result=mysqli_query($conn,$sql);
+// if(mysqli_num_rows($result)>0){
+//     $user_id=mysqli_arra($result);
+// }else{
+//     echo "Something went wrong";
+// }
+// fetching user id from db
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $user_id = $row["id"];
+    }
+  } else {
+    echo "0 results";
+  }
+$cost = strval($_COOKIE['price']);
+$adults = strval($_COOKIE['num_of_adults']);
+$children = strval($_COOKIE['num_of_children']);
+$date_from = strval($_COOKIE['check_in']);
+$days = strval($_COOKIE['num_of_days']);
+$date_till = date('Y-m-d', strtotime($date_from. ' + '.$days.' days'));
+$food_type = strval($_COOKIE['food_type']);
+$query = "INSERT INTO `booking`(`tour_package_id`, `user_id`, `cost`, `adults`, `children`, `date_from`, `date_till`, `food_type`) VALUES ($tour_package_id,$user_id,$cost,$adults,$children,$date_from,$date_till,'$food_type');";
+if ($conn->query($query) === TRUE) {
+    echo "<script>console.log(Payment Successful);</script>";
+  } else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+  }
+}
+add_booking_order();
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
