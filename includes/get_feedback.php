acae8875f29afcf98f52a223d85fb6b58b5e2703
AxonBlAzE <s.mulkutkar@somaiya.edu>
2021-11-15 11:59:04 +0530

    minor fixes

--- includes/get_feedback.php
+++ includes/get_feedback.php
@@ -39,6 +39,37 @@ function get_random_number(){
     }
 }
 
+function three_non_equal_random_number(){
+
+}
+
+//function to get 3 random non equal numbers
+function three_random_number(){
+    $cn = new mysqli("localhost","root","","travels","3306");
+    $query = "SELECT * FROM `feedback`";
+    $result=mysqli_query($cn,$query);
+    $i = 0;
+    if(mysqli_num_rows($result)>0){
+        while($row = $result->fetch_assoc()) {
+            $i++;
+        }
+        $n1 = rand(0,$i-1);
+        $n2 = rand(0,$i-1);
+        $n3 = rand(0,$i-1);
+        while($n1==$n2 || $n1==$n3 || $n2==$n3){
+            $n1 = rand(0,$i-1);
+            $n2 = rand(0,$i-1);
+            $n3 = rand(0,$i-1);
+        }
+        $numbers = array($n1,$n2,$n3);
+        return $numbers;
+        }
+    else{
+        // echo "Something went wrong";
+        return false;
+    }
+}
+
 function get_feedback_by_id($id) {
     $cn = new mysqli("localhost","root","","travels","3306");
     $query = "SELECT * FROM `feedback`";
